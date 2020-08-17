01f51d0c056ea7c4626a1c78d9956fbf79b7aaf4
Lucas Saliés Brum <lucas@archlinux.com.br>
2020-08-17 17:47:01 -0400

    Autocommit por GitHub: alterações: add 'app/Model/Template.php' add 'app/Config/autoload.php'

---
+++ app/Config/autoload.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * https://github.com/php-fig/fig-standards/blob/master/accepted/PSR-4-autoloader-examples.md
+*/
+
+spl_autoload_register(function ($class) {
+    $prefix = 'App\\';
+    $base_dir = APP;
+
+    $len = strlen($prefix);
+    if (strncmp($prefix, $class, $len) !== 0) {
+        return;
+    }
+
+    $relative_class = substr($class, $len);
+    $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
+
+    if (file_exists($file)) {
+        require $file;
+    }
+});
