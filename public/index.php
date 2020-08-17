f7b7161bb7601bfe1d854680724d242258bae724
Lucas Saliés Brum <lucas@archlinux.com.br>
2020-08-17 18:19:02 -0400

    Autocommit por GitHub: alterações: add 'public/index.php'

--- public/index.php
+++ public/index.php
@@ -3,5 +3,8 @@
 define('ROOT', dirname(__DIR__) . DIRECTORY_SEPARATOR);
 define('APP', ROOT . 'app' . DIRECTORY_SEPARATOR);
 define('CONFIG', APP . 'Config' . DIRECTORY_SEPARATOR);
+define('TEMPLATES', ROOT . 'templates' . DIRECTORY_SEPARATOR);
+
 require CONFIG . 'autoload.php';
-$app = new App\Core\Application();
+
+$app = new App\Core\App();
