f255d762847b135deaffb7699cda4fa0c0f2b279
Lucas Saliés Brum <lucas@archlinux.com.br>
2020-08-17 18:25:01 -0400

    Autocommit por GitHub: alterações: add 'app/Controller/TemplatesController.php'

--- app/Controller/TemplatesController.php
+++ app/Controller/TemplatesController.php
@@ -1,31 +1,12 @@
 <?php
 
 namespace App\Controller;
-use App\Model\Frete;
+use App\Model\Template;
 
-class PaginasController
+class TemplatesController
 {
     public function index()
     {
-        $Frete = new Frete();
-        $fretes = $Frete->listar();
-        $contagem = $Frete->contagem();
-        require APP . 'View/_templates/header.php';
-        require APP . 'View/paginas/index.php';
-        require APP . 'View/_templates/footer.php';
-    }
-    
-    public function busca()
-    {
-        require APP . 'View/_templates/header.php';
-        require APP . 'View/paginas/busca.php';
-        require APP . 'View/_templates/footer.php';
-    }
-
-    public function erro()
-    {
-        require APP . 'View/_templates/header.php';
-        require APP . 'View/paginas/erro.php';
-        require APP . 'View/_templates/footer.php';
+        echo Template::render(TEMPLATES . 'index.html');
     }
 }
